b5e2f55109d01eb1bbe08acee119c6de88c10a61
Rohan Prabhu <rohan.prabhu@manipal.net>
2020-07-21 23:17:17 +0530

    removed shared pref import

--- lib/bloc/boredBloc.dart
+++ lib/bloc/boredBloc.dart
@@ -2,7 +2,6 @@ import 'package:bored/services/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:bored/services/json_service.dart';
 import 'package:bored/services/service_locator.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class BoredBloc with ChangeNotifier {
   JsonService _jsonService = locator<JsonService>();
